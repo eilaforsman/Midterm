1519347274cf6999e7338b2a9e1c1e36a7be0285
Eila Forsman <Eila@Eilas-MacBook-Air.local>
2022-12-01 14:38:53 +0100

    Script

--- RScript_FDA.R
+++ RScript_FDA.R
@@ -24,9 +24,9 @@ summary(dataFDA)
 
 
 library(plyr)
-dataFDA_sub <- subset(dataFDA, Lokal != "H_Pålsjö_efter_HW")
+dataFDA_sub <- subset(dataFDA, Lokal != "H_Pålsjö_efter_HW") #Remove "efter_HW" because its a different treatment
 mean_LokalFDA<-ddply(dataFDA_sub,"Lokal",summarise,
-                  mean_antal=mean(Antal))
+                  mean_antal=mean(Antal)) #Summarize data by site
 
 
 head(dataFDA_sub)
@@ -46,15 +46,16 @@ dataFDA_sub$ID_spec <- ID_split$V1
 paste(dataFDA_sub$Lokal, dataFDA_sub$ID_spec, sep ="_")
 dataFDA_sub$Prov<-paste(dataFDA_sub$Lokal, dataFDA_sub$ID_spec, sep ="_")
 head(dataFDA_sub)
-
+#Created new column of sample without sample ID
 
 Prov<-ddply(dataFDA_sub, "Prov", summarise, mean_antal=mean(Antal))
 head(Prov)
 
 nchar(dataFDA_sub$Prov)
-dataFDA_sub$newprov <- substr(dataFDA_sub$Prov, 1, nchar(dataFDA_sub$Prov) - 2)
-Meantot<-ddply(dataFDA_sub, "newprov", summarise, mean_tot=mean(Antal))
-Meantot$order <- c(4,5,1,9,10,11,12,13,14,2,8,6,7,3)
+dataFDA_sub$newprov <- substr(dataFDA_sub$Prov, 1, nchar(dataFDA_sub$Prov) - 2) #New column of sites
+
+Meantot<-ddply(dataFDA_sub, "newprov", summarise, mean_tot=mean(Antal)) #Calculate mean number per site
+Meantot$order <- c(4,5,1,9,10,11,12,13,14,2,8,6,7,3) #Set order of sites for later plotting
 
 #Basic Plotting####
 
@@ -85,6 +86,8 @@ summary(m)
 
 #Site test#####
 
+#Is there a difference in number of live cells between the Heatweed treated sites?
+#Do we need to take site into account in the model?
 
 kruskal.test(dataFDA_sub$Antal ~ dataFDA_sub$Prov) #Big difference between root samples
 
@@ -107,7 +110,7 @@ library(lmerTest)
 ?lme4
 ?lmerTest
 
-#LMM förutsätter normalfördelning
+#LMM assumes normal distribution
 #m3 = lmer(response ~ fixed factor + ev fixed 2+ fixed factor 1* fixed 2 + (1|random), data=data_sub)
 
 
@@ -115,7 +118,7 @@ library(lmerTest)
 #m3 = glmer(response ~ fixed factor + ev fixed 2+ fixed factor 1* fixed 2 + (1|random), family= "poisson", data=data_sub)
 
 #Assumes equal variances between groups, (levene.test()), sig is problem 
-#Om anova inte funkar -> testa Anova
+#If anova doesn't work -> try Anova
 
 
 m2 = glm.nb(Antal ~ Behandling, data=dataFDA_sub)
@@ -151,13 +154,13 @@ boxplot(resid(m5) ~ dataFDA_sub$Behandling)
 m6 = glmer.nb(Antal ~ Behandling + (1|Lokal), family="poisson", data=dataFDA_sub)
 m7 = glmmTMB(Antal ~ Behandling + (1|Lokal/Prov), family="nbinom1", data=dataFDA_sub)
 summary(m6) 
-summary(m7)#Includes site and sample as random factor and correct overdispersion, 
+summary(m7)#Includes site and sample as random factor and corrects for overdispersion, 
              #correct data distribution
 
 boxplot(resid(m6) ~ dataFDA_sub$Behandling)
 boxplot(resid(m7) ~ dataFDA_sub$Behandling)
 
-plot(Antal ~ Behandling, data=dataFDA_sub)
+plot(Antal ~ Behandling, data=dataFDA_sub) #Visualize data distribution between treatments
 
 #Results####
 
@@ -182,12 +185,12 @@ head(Meantot)
 par(mfrow=c(1,1))
 ?ggplot()
 
-
+#Mean number of live cells per micrograph####
 
 FDAMeans<-ddply(dataFDA_sub,"Lokal", summarize, N=length(Antal),
                          mean.antal=mean(na.omit(Antal)),
                         sd.FDA=sd(na.omit(Antal)),
-                         se.FDA=sd.FDA/sqrt(N))
+                         se.FDA=sd.FDA/sqrt(N)) #Summarize data per site with sd and se for creating error bars
 
 ggplot(Meantot, aes(x=reorder(Meantot$newprov, Meantot$order), y=Meantot$mean_tot)) +
         geom_bar(width = 0.75, stat = "identity", position ="dodge", alpha = 0.8) +
@@ -215,18 +218,18 @@ ggsave("Celler_FDA_plot.png", plot = last_plot(), device = "png",
 SampleMeans <- ddply(dataFDA_sub,"Prov", summarize, N=length(Antal),
                      mean.antal=mean(na.omit(Antal)),
                      sd.FDA=sd(na.omit(Antal)),
-                     se.FDA=sd.FDA/sqrt(N))
+                     se.FDA=sd.FDA/sqrt(N)) #Create summarize of number of cells per sample with means, sd and se.
 str(SampleMeans)
 SampleMeans$Prov = as.character(SampleMeans$Prov)
 
 
-SampleMeans$newprov <- substr(SampleMeans$Prov, 1, nchar(SampleMeans$Prov) - 2)
+SampleMeans$newprov <- substr(SampleMeans$Prov, 1, nchar(SampleMeans$Prov) - 2)#New column of sites without ID
 
 Sample = aggregate(cbind(SampleMeans$mean.antal, SampleMeans$sd.FDA, 
                          SampleMeans$se.FDA), 
-                   by=list(newprov=SampleMeans$newprov), FUN =sum)
+                   by=list(newprov=SampleMeans$newprov), FUN =sum) #New data frame from summary with same length of data 
 
-
+#Add sample summary data to Meantot (Total means) data frame
 Meantot$mean_sample = Sample$V1
 Meantot$sd = Sample$V2
 Meantot$se = Sample$V3
@@ -254,19 +257,19 @@ ggsave("Celler_prov_plot.png", plot = last_plot(), device = "png",
 
 #Variance####
 
-vardat = split(dataFDA_sub, f=dataFDA_sub$Behandling)
+#Split data according to treatment
+vardat = split(dataFDA_sub, f=dataFDA_sub$Behandling) 
 datC = vardat$Kontroll
 datHW = vardat$Heatweed
 
-hist(datC$Antal)
+hist(datC$Antal) #Visualize Control data
 
-m = glm.nb(Antal ~ 1, data=datC)
+m = glm.nb(Antal ~ 1, data=datC) #Fit model to control data
 summary(m)
 
-hist(datHW$Antal)
+hist(datHW$Antal)#Visualize HW data
 
-
-m2 = glm.nb(Antal ~ 1, data=datHW)
+m2 = glm.nb(Antal ~ 1, data=datHW) #Fit model to HW data
 summary(m2)
 
 coefs = summary(m)$coef
